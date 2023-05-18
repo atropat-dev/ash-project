0f0b85521e94ac94c94d786f54ec2e382010220d
Hossein Mohammadi Sardari <hosseinmohammadidev@gmail.com>
2023-05-18 22:38:05 +0330

    create new dark / light theme

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import "./App.css"
+import "./styles/light.css"
+import "./styles/dark.css"
 import Button from "@mui/material/Button"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Feed from "./components/Feed"
+import { createContext } from "react"
+import { useState } from "react"
+
+export const ThemeContext = createContext(null)
 
 function App() {
+  const [theme, setTheme] = useState("light")
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"))
+  }
   return (
-    <div className="App">
-      <Header />
-      <Feed />
-      <Footer />
-    </div>
+    <ThemeContext.Provider value={(theme, setTheme)}>
+      <div className="App" id={theme}>
+        <Header />
+        <Feed />
+        <Footer />
+      </div>
+    </ThemeContext.Provider>
   )
 }
-
 
 export default App
